62657a4b0ae7a4d2a890e8fbd7fd51b8ad905510
bobetbat <b.bobetayev.97@gmail.com>
2022-12-06 13:45:47 +0100

    feat: welcome page

--- src/components/RequireConnect.tsx
+++ src/components/RequireConnect.tsx
@@ -1,19 +1,31 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { useAccount } from 'wagmi';
-import { Box } from '@mui/joy';
-import { Message } from '../components/Message';
+import { Box, Card, Stack, Typography } from '@mui/joy';
 
 export const RequireConnect = () => {
   const { address } = useAccount();
-
+  if (address) return null;
   return (
-    <>
-      <Message type="warn" show={address === undefined}>
-        Please connect your wallet to continue
-      </Message>
-      <Box sx={{ mb: 2 }}>
-        <ConnectButton chainStatus={'none'} showBalance={false} />
-      </Box>
-    </>
+    <Stack direction="column" alignItems="center" maxWidth="100%">
+      <Stack width="30rem">
+        {/* <AspectRatio sx={{ width: 300, borderRadius: 'md', overflow: 'auto' }}>
+                <img src={profileData.logo} />
+              </AspectRatio> */}
+        <Typography>
+          Migrate your ORGiD benefit from last developments and be ready for the
+          marketplace launch
+        </Typography>
+        <Card sx={{ alignItems: 'center', p: 4, mt: 5, mb: 5, border: '1px solid #000' }}>
+          <Typography fontSize="2.5em">Migrate your ORGiD</Typography>
+          <Box sx={{ mt: 3 }}>
+            <ConnectButton chainStatus={'none'} showBalance={false} />
+          </Box>
+        </Card>
+        <>
+          <Typography>Discord</Typography>
+          <Typography>Read more about Winding Tree protocol</Typography>
+        </>
+      </Stack>
+    </Stack>
   );
 };
